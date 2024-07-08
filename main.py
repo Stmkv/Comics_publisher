42812838150e4cdae1418d66f7bafe36a91200ed
Stmkv <stepkamakarov1999@mail.ru>
2024-07-08 14:14:45 +0300

    Написал функцию  public_comics
    Функция нужна для отправки картинки и сообщения в телеграмм канал

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import requests
 import random
+import telegram
+import os
+
+from dotenv import load_dotenv
 from upload_picture import download_picture
 
 
-def download_comics() -> None:
-    url = "https://xkcd.com/353/info.0.json"
+def download_comics():
+    comics_number = random.randint(1, 2955)
+    url = f"https://xkcd.com/{comics_number}/info.0.json"
     response = requests.get(url)
     response.raise_for_status()
     response = response.json()
     img_url = response['img']
     comics_comment = response['alt']
 
-    download_picture(img_url, '1.png')
-    print(comics_comment)
+    file_path = f'comics/{comics_number}.png'
+    download_picture(img_url, file_path)
+    return comics_comment, file_path
 
 
-download_comics()
+def public_comics(bot, chat_id, comments, photo_path):
+    bot.send_message(chat_id, comments)
+    with open(photo_path, 'rb') as photo:
+        bot.send_photo(chat_id, photo)
+    os.remove(photo_path)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    telegram_bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
+    telegram_chat_id = os.environ["TELEGRAM_CHAT_ID"]
+    bot = telegram.Bot(token=telegram_bot_token)
+    comics_comment, photo_path = download_comics()
+    public_comics(bot, telegram_chat_id, comics_comment, photo_path)
