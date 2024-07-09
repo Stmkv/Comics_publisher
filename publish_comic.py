4e8332c0e2c904460787fef1ca1e0dd22807e95c
Stmkv <stepkamakarov1999@mail.ru>
2024-07-09 10:58:18 +0300

    Изменил название функции. Добавил совместимость

--- publish_comic.py
+++ publish_comic.py
@@ -7,8 +7,11 @@ from dotenv import load_dotenv
 from upload_picture import download_picture
 
 
+LAST_COMIC = 2955
+
+
 def download_comic():
-    comics_number = random.randint(1, 2955)
+    comics_number = random.randint(1, LAST_COMIC)
     url = f"https://xkcd.com/{comics_number}/info.0.json"
     response = requests.get(url)
     response.raise_for_status()
@@ -16,12 +19,12 @@ def download_comic():
     img_url = response['img']
     comics_comment = response['alt']
 
-    file_path = f'comics/{comics_number}.png'
+    file_path = os.path.join('comics', f'{comics_number}.png')
     download_picture(img_url, file_path)
     return comics_comment, file_path
 
 
-def public_comics(bot, chat_id, comments, photo_path):
+def publish_comic(bot, chat_id, comments, photo_path):
     bot.send_message(chat_id, comments)
     with open(photo_path, 'rb') as photo:
         bot.send_photo(chat_id, photo)
@@ -35,4 +38,4 @@ if __name__ == '__main__':
     bot = telegram.Bot(token=telegram_bot_token)
     os.makedirs("Comics", exist_ok=True)
     comics_comment, photo_path = download_comic()
-    public_comics(bot, telegram_chat_id, comics_comment, photo_path)
+    publish_comic(bot, telegram_chat_id, comics_comment, photo_path)
